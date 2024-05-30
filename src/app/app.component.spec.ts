cbea083e2dd7a6f1cd350c715149da559bcc1b44
Daniela Zaffalon <daniela.zaffalon@onemindng.com>
2024-05-30 17:25:04 +0200

    Adding Firebase to the Project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'locationsCrud' title`, () => {
+  it(`should have the 'locationscrud' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('locationsCrud');
+    expect(app.title).toEqual('locationscrud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, locationsCrud');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, locationscrud');
   });
 });
